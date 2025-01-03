d1b8c11634dbcf5332038227158828bc81f58774
Kumud Gupta <40192898+kumudgupta76@users.noreply.github.com>
2025-01-03 21:31:26 +0530

    Updates

--- src/components/NoPage.jsx
+++
@@ -1,9 +0,0 @@
-import { getCurrentUser } from "../common/authUtils";
-
-const NoPage = () => {
-    console.log(process.env);
-    const user = getCurrentUser();
-    return <h1>404 {process.env.APP_NAME} - {JSON.stringify(user)}</h1>;
-  };
-  
-  export default NoPage;
